dab4120862e5230296d04682ea8076410726f42a
Ruslan Maksymchuk <stra1ker.usa@gmail.com>
2024-06-30 10:19:29 +0300

    number_1

--- homework_core_mode_3_num2.py
+++ homework_core_mode_3_num2.py
@@ -1,2 +1,11 @@
+import random
+
 def get_numbers_ticket(min, max, quantity):
-    
+    if min < 1 or max > 1000 or quantity < 1 or quantity > (max - min + 1):
+        return []
+
+    numbers = random.sample(range(min, max + 1), quantity)
+    numbers.sort()
+    return numbers
+
+print(get_numbers_ticket(1, 1000, 6))
