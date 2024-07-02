729a23bd04e86aac239da250b35e512c2d777a47
Ruslan Maksymchuk <stra1ker.usa@gmail.com>
2024-07-02 09:45:16 +0300

    correction_error

--- homework_core_mode_3.py
+++ homework_core_mode_3.py
@@ -1,9 +1,12 @@
 from datetime import datetime
 
 def get_days_from_today(date):
-    date_transformation = datetime.strptime(date, "%Y-%m-%d")
-    today = datetime.today()
-    difference = today - date_transformation
-    return difference.days
+    try
+        date_transformation = datetime.strptime(date, "%Y-%m-%d")
+        today = datetime.today()
+        difference = today - date_transformation
+        return difference.days
+    except ValueError:
+        return "Невірний фортат дати. 'Введіть РРРР-мм-дд'"
 date = "2024-06-19"
 print(get_days_from_today(date))
